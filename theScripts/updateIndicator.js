eeef1cee7aad8aa18762abb951842c32426d13be
mrx <albin@senioradministrator.com>
2022-08-03 01:56:34 +0200

    part of code indicator is divided into this file for use in MyIndicator.qml

---
+++ theScripts/updateIndicator.js
@@ -0,0 +1,8 @@
+
+function setIndexActive(a_Active,b_Diactive,c_Diactive,d_Diactive)
+{
+    a_Active.color = cBG_button;
+    b_Diactive.color = cBG_button_deactivated;
+    c_Diactive.color = cBG_button_deactivated;
+    d_Diactive.color = cBG_button_deactivated;
+}
